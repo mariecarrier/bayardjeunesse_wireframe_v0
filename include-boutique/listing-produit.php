e325b926ff2ad78ec0ca025245ac1882904acce9
unknown <mm.carrier@A12LP1478.bayardcanada.com>
2015-05-04 11:49:56 -0400

    test encore

---
+++ include-boutique/listing-produit.php
@@ -0,0 +1,24 @@
+
+    <div class="col-lg-3 image-produit">
+        <div class="wrapper-cover-content">
+            <div class="margin-cover-content">
+                <div class="wrapper-covers-content">
+                    <div class="table-w">
+                           <img src="img/deb_cover.jpg" style=" width: 100%;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-listing">
+            <h2>Les Débrouillards</h2>
+            <div class="sub regular age">9 à 14 ans</div>
+            <div class="sub plus">41.95$</div>
+            <div class="wrap-large-but" id="je-labonne">
+                <a href="/page-produit.php"><div class="subscribenow content bouton-achat"  id="je-labonne">
+                    <input type="button" value="Je l'abonne" class="call"/>
+                </div></a>
+            </div>
+        </div>
+    </div>
+
+
---
+++ include-transact/lp-produits.php
@@ -0,0 +1,24 @@
+
+
+
+<?php
+        include 'breadcrump.php';
+?> 
+
+<div class="row content1"> 
+<?php 
+include 'listing-produit.php';
+
+include 'listing-produit.php';
+
+include 'listing-produit.php';
+
+include 'listing-produit.php';
+
+include 'listing-produit.php';
+
+include 'listing-produit.php';
+?>
+</div> 
+
+
